459ab9e3c52486a9801c91c057bded8d9526d6cc
Pavel <Pivkin.Pavel.11@yandex.ru>
2015-04-30 14:51:06 +0300

    ok

--- ask_Pivkin/urls.py
+++ ask_Pivkin/urls.py
@@ -5,6 +5,11 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'ask_Pivkin.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^helloworldapp/', 'ask_Pivkin.views.helloworldapp'),
+    url(r'^$', 'ask_Pivkin.views.index'),
+    url(r'^registration/', 'ask_Pivkin.views.signup'),
+    url(r'^login', 'ask_Pivkin.views.login'),
+    url(r'^newquestion/', 'ask_Pivkin.views.ask'),	
+    url(r'^question/', 'ask_Pivkin.views.question'),
     url(r'^admin/', include(admin.site.urls)),
 ]
